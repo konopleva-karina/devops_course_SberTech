40bbe75490a41b273876cb02078908fa023f8e90
Konopleva Karina <konopleva2001karina@gmail.com>
2022-12-08 23:16:49 +0300

    fix

--- hello/src/test/java/org/example/CalculatorTest.java
+++ hello/src/test/java/org/example/CalculatorTest.java
@@ -3,11 +3,11 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CalculatorTest {
+class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
-    public void testAdd() {
+    void testAdd() {
         Assertions.assertEquals(5, calculator.add(3, 2));
     }
 }
