60fbf41bb499658efb2db832353f0c1e82c54269
Konopleva Karina <konopleva2001karina@gmail.com>
2022-12-12 02:37:38 +0300

    fix

---
+++ hello/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assertions.assertEquals(5, calculator.add(3, 2));
+    }
+}
